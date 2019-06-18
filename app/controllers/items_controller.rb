f59c2f20243d632ab46331177575f30827895a1b
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-06-18 18:27:32 -0400

    added an edit feature and a delete button

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,4 +10,12 @@ class ItemsController < ApplicationController
     render json: @item
   end
 
+  def update
+    @item = Item.find_by(id: params[:id])
+    @item.update(name: params[:name], oneliner: params[:oneliner], description: params[:description], section: params[:section], picture: params[:picture])
+    @item = Item.find_by(id: params[:id])
+    byebug
+    render json: @item
+  end
+
 end
