9c4d6962c436181b9cf4f5c5e7cb7beec94cc60e
joshuamsmith413 <joshuamsmith413@gmail.com>
2019-08-13 23:58:09 -0400

    added restaurant tiles to homepage instead of dropdown on navbar and added picture column to restaurant table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_054111) do
+ActiveRecord::Schema.define(version: 2019_08_13_180724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_06_08_054111) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
